deef461277dc12511394c0d0a8741ecd8e4772f6
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-03-03 13:59:13 +0800

    [api] Add further configuration options
    Add imports and parsing `MANAGEMENT_SECRET` (used to authenticate management endpoints) and `JWT_SECRET` (used to sign JWT)

--- api/src/commons/config.rs
+++ api/src/commons/config.rs
@@ -1,13 +1,19 @@
 use std::env;
 
+use anyhow::{Context, Result, anyhow};
+use base64::prelude::{BASE64_URL_SAFE_NO_PAD, Engine};
+use hex::FromHex;
+
 pub struct Config {
     pub database_url: String,
     pub port: String,
+    pub management_secret: [u8; 64],
+    pub jwt_secret: [u8; 32],
 }
 
 impl Config {
     #[allow(clippy::missing_errors_doc)] // Error is well understood and intentional.
-    pub fn new() -> anyhow::Result<Self> {
+    pub fn new() -> Result<Self> {
         // Retrieve DATABASE_URL
         let database_url = match env::var("DATABASE_URL") {
             Ok(database_url) => {
@@ -30,6 +36,44 @@ impl Config {
             "3000".to_string()
         };
 
-        Ok(Self { database_url, port })
+        // Retrieve MANAGEMENT_SECRET
+        let management_secret: [u8; 64] = match env::var("MANAGEMENT_SECRET") {
+            Ok(management_secret) => {
+                tracing::info!("MANAGEMENT_SECRET found.");
+                tracing::debug!("MANAGEMENT_SECRET set to {}.", management_secret);
+                BASE64_URL_SAFE_NO_PAD
+                    .decode(management_secret.as_bytes())
+                    .context("Unable to decode MANAGEMENT_SECRET.")?
+                    .try_into()
+                    .map_err(|_| anyhow!("MANAGEMENT_SECRET is not 64 bytes long."))?
+            }
+            Err(e) => {
+                tracing::error!("MANAGEMENT_SECRET not set. Exiting.");
+                return Err(e.into()); // Error here.
+            }
+        };
+
+        // Retrieve JWT_SECRET
+        let jwt_secret: [u8; 32] = match env::var("JWT_SECRET") {
+            Ok(jwt_secret) => {
+                tracing::info!("JWT_SECRET found.");
+                tracing::debug!("JWT_SECRET set to {}.", jwt_secret);
+                Vec::from_hex(jwt_secret)
+                    .context("Unable to decode JWT_SECRET.")?
+                    .try_into()
+                    .map_err(|_| anyhow!("JWT_SECRET is not 32 bytes long."))?
+            }
+            Err(e) => {
+                tracing::error!("JWT_SECRET not set. Exiting.");
+                return Err(e.into()); // Error here.
+            }
+        };
+
+        Ok(Self {
+            database_url,
+            port,
+            management_secret,
+            jwt_secret,
+        })
     }
 }
